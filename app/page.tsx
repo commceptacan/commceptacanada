e8adc3fdd81d0b26ed1958e9662610d40a547c6f
commceptacan <commceptacanada@gmail.com>
2025-01-15 10:16:38 -0500

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import styles from "./page.module.scss";
 import Image from "next/image";
 //images
 import logo from "./../public/imgs/commcepta.png";
+import mountain from "./../public/imgs/mountain.png";
 
 export default function Home() {
   return (
     <main className={styles.page}>
+      <Image className={styles.page_mountain} src={mountain} alt="logo" />
       <Image className={styles.page_logo} src={logo} alt="logo" />
     </main>
   );
